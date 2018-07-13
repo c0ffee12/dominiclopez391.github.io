22789d90cea25b7e04661a2a0d9fce85a0482726
Dominic <dominiclopez391@gmail.com>
2018-07-13 16:03:07 -0700

    guess, fixed broken picture loop

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,23 @@
-var myImage = document.querySelector('img');
+
 var myHeader = document.querySelector('h1');
 var myButton = document.querySelector('button');
-myImage.onclick = function() {
-	
-	var mySrc = myImage.getAttribute('src');
-	if(mySrc === 'images/dog1.jpg') {
-		myImage.setAttribute('src', 'images/dog2.jpeg');
-	} else {
-		myImage.setAttribute('src', 'images/dog1.jpg');
+var myImages = document.querySelectorAll('img');
+
+for(var i = 0; i < myImages.length; i++) {
+	myImages[i].onclick = function() {
+		mySrc = this.getAttribute('src');
+		if(mySrc === 'images/dog1.jpg') {
+			this.setAttribute('src', 'images/dog2.jpeg');
+			this.setAttribute('title', 'goodboy2');
+		} else {
+			this.setAttribute('src', 'images/dog1.jpg');
+			this.setAttribute('title', 'goodboy1');
+		}
 	}
-	
 }
 
 //set header
-if(!localStorage.getItem('name')) {
+if(!localStorage.getItem('name') || localStorage.getItem('name') === 'null') {
 	setUserName();
 }
 else {
@@ -31,3 +35,36 @@ function setUserName() {
 myButton.onclick = function() {
 	setUserName();
 }
+
+//guessGame
+var guessField = document.querySelector('.guessField');
+var guessSubmit = document.querySelector('.guessSubmit');
+var randomValue = Math.floor(Math.random() * 100) + 1;
+var winCondition = document.querySelector('.winCondition');
+
+guessSubmit.addEventListener('click', checkGuess);
+
+function checkGuess() {
+	if(Number(guessField.value) < randomValue) {
+		winCondition.textContent = 'Guess Is Low';
+		document.getElementsByClassName('winCondition')[0].style.color = "blue";
+		guessField.value = '';
+		guessField.focus();
+	} else if(Number(guessField.value) > randomValue) {
+		winCondition.textContent = 'Guess Is High';
+		document.getElementsByClassName('winCondition')[0].style.color = "red";
+		guessField.value = '';
+		guessField.focus();
+	}
+	else if (Number(guessField.value) === randomValue) {
+		winCondition.textContent = 'You Win!';
+		guessField.setAttribute('disabled', true);
+		document.getElementsByClassName('winCondition')[0].style.color = "yellow";
+	}
+	else {
+		winCondition.textContent = 'Numbers Only!';
+		document.getElementsByClassName('winCondition')[0].style.color = "black";
+	}
+}
+
+
